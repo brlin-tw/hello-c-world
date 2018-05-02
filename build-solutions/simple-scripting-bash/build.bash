16e70862f740eb840381718e000089c7afc0baaa
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-03 00:44:40 +0800

    build-solutions/simple-scripting-bash/build.bash: Rebase on new template version
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- build-solutions/simple-scripting-bash/build.bash
+++ build-solutions/simple-scripting-bash/build.bash
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
-
 # Script to build the software
 # 林博仁 © 2018
-
-for critical_command in \
-	basename\
-	dirname\
-	realpath
-	do
-	if ! command -v "${critical_command}" &> /dev/null; then
-		echo 'Fatal: This program requires GNU Coreutils in the executable search path'
-		exit 1
-	fi
-done
 
 ## Makes debuggers' life easier - Unofficial Bash Strict Mode
 ## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
@@ -21,6 +8,48 @@ set -o errexit
 set -o errtrace
 set -o nounset
 set -o pipefail
+
+## Runtime Dependencies Checking
+declare\
+	runtime_dependency_checking_result=still-pass\
+	required_software
+
+for required_command in \
+	basename \
+	dirname \
+	gcc \
+	realpath; do
+	if ! command -v "${required_command}" &>/dev/null; then
+		runtime_dependency_checking_result=fail
+
+		case "${required_command}" in
+			basename \
+			|dirname \
+			|realpath)
+				required_software='GNU Coreutils'
+				;;
+			gcc)
+				required_software='GCC, the GNU Compiler Collection'
+				;;
+			*)
+				required_software="${required_command}"
+				;;
+		esac
+
+		printf -- \
+			'Error: This program requires "%s" to be installed and its executables in the executable searching paths.\n' \
+			"${required_software}" \
+			1>&2
+		unset required_software
+	fi
+done; unset required_command required_software
+
+if [ "${runtime_dependency_checking_result}" = fail ]; then
+	printf -- \
+		'Error: Runtime dependency checking fail, the progrom cannot continue.\n' \
+		1>&2
+	exit 1
+fi; unset runtime_dependency_checking_result
 
 ## Non-overridable Primitive Variables
 ## BASHDOC: Shell Variables » Bash Variables
@@ -31,28 +60,28 @@ if [ -v 'BASH_SOURCE[0]' ]; then
 	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
 	RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
 	RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
-	declare -r\
-		RUNTIME_EXECUTABLE_FILENAME\
-		RUNTIME_EXECUTABLE_DIRECTORY\
-		RUNTIME_EXECUTABLE_PATHABSOLUTE\
+	# We intentionally leaves these variables for script developers
+	# shellcheck disable=SC2034
+	declare -r \
+		RUNTIME_EXECUTABLE_PATH \
+		RUNTIME_EXECUTABLE_FILENAME \
+		RUNTIME_EXECUTABLE_NAME \
+		RUNTIME_EXECUTABLE_DIRECTORY \
 		RUNTIME_COMMANDLINE_BASECOMMAND
 fi
-declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
+declare -ar RUNTIME_COMMANDLINE_ARGUMENTS=("${@}")
 
 ## init function: entrypoint of main program
 ## This function is called near the end of the file,
 ## with the script's command-line parameters as arguments
 init(){
-	if ! process_commandline_parameters; then
-		printf\
-			'Error: %s: Invalid command-line parameters.\n'\
-			"${FUNCNAME[0]}"\
+	if ! process_commandline_arguments; then
+		printf -- \
+			'Error: Invalid command-line parameters.\n' \
 			1>&2
-		print_help
-		exit 1
-	fi
 
-	if ! check_runtime_dependencies; then
+		printf '\n' # separate error message and help message
+		print_help
 		exit 1
 	fi
 
@@ -92,10 +121,68 @@ init(){
 	exit 0
 }; declare -fr init
 
+print_help(){
+	printf \
+		'Currently no help messages are available for this program\n' \
+		1>&2
+	return 0
+}; declare -fr print_help;
+
+process_commandline_arguments() {
+	if [ "${#RUNTIME_COMMANDLINE_ARGUMENTS[@]}" -eq 0 ]; then
+		return 0
+	fi
+
+	# Modifyable parameters for parsing by consuming
+	local -a parameters=("${RUNTIME_COMMANDLINE_ARGUMENTS[@]}")
+
+	# Normally we won't want debug traces to appear during parameter parsing, so we add this flag and defer its activation till returning(Y: Do debug)
+	local enable_debug=N
+
+	while true; do
+		if [ "${#parameters[@]}" -eq 0 ]; then
+			break
+		else
+			case "${parameters[0]}" in
+				--help \
+				|-h)
+					print_help;
+					exit 0
+					;;
+				--debug \
+				|-d)
+					enable_debug=Y
+					;;
+				*)
+					printf -- \
+						'%s: Error: Unknown command-line argument "%s"\n' \
+						"${FUNCNAME[0]}" \
+						"${parameters[0]}" \
+						>&2
+					return 1
+					;;
+			esac
+			# shift array by 1 = unset 1st then repack
+			unset 'parameters[0]'
+			if [ "${#parameters[@]}" -ne 0 ]; then
+				parameters=("${parameters[@]}")
+			fi
+		fi
+	done
+
+	if [ "${enable_debug}" = Y ]; then
+		trap 'trap_return "${FUNCNAME[0]}"' RETURN
+		set -o xtrace
+	fi
+	return 0
+}; declare -fr process_commandline_arguments
+
 ## Traps: Functions that are triggered when certain condition occurred
 ## Shell Builtin Commands » Bourne Shell Builtins » trap
 trap_errexit(){
-	printf 'An error occurred and the script is prematurely aborted\n' 1>&2
+	printf \
+		'An error occurred and the script is prematurely aborted\n' \
+		1>&2
 	return 0
 }; declare -fr trap_errexit; trap trap_errexit ERR
 
@@ -106,107 +193,25 @@ trap_exit(){
 trap_return(){
 	local returning_function="${1}"
 
-	printf 'DEBUG: %s: returning from %s\n' "${FUNCNAME[0]}" "${returning_function}" 1>&2
+	printf \
+		'DEBUG: %s: returning from %s\n' \
+		"${FUNCNAME[0]}" \
+		"${returning_function}" \
+		1>&2
 }; declare -fr trap_return
 
 trap_interrupt(){
 	printf '\n' # Separate previous output
-	printf 'Recieved SIGINT, script is interrupted.' 1>&2
+	printf \
+		'Recieved SIGINT, script is interrupted.' \
+		1>&2
 	return 1
 }; declare -fr trap_interrupt; trap trap_interrupt INT
-
-print_help(){
-	printf 'Currently no help messages are available for this program\n' 1>&2
-	return 0
-}; declare -fr print_help;
-
-check_runtime_dependencies(){
-	## Runtime Dependencies Checking
-	declare\
-		runtime_dependency_checking_result='still-pass' \
-		required_software
-
-	# Only an item to check for now
-	# shellcheck disable=SC2043
-	for required_command in \
-		gcc
-		do
-		if ! command -v "${required_command}" &>/dev/null; then
-			runtime_dependency_checking_result='fail'
-
-			case "${required_command}" in
-				gcc)
-					required_software='GCC, the GNU Compiler Collection'
-				;;
-				*)
-					required_software="${required_command}"
-				;;
-			esac
-
-			printf --\
-				"Error: This program requires \"%s\" to be installed and it's executables in the executable searching paths.\\n"\
-				"${required_software}" 1>&2
-			unset required_software
-		fi
-	done; unset required_command required_software
-
-	if [ "${runtime_dependency_checking_result}" = 'fail' ]; then
-		printf --\
-			'Error: Runtime dependency checking fail, the progrom cannot continue.\n' 1>&2
-		return 1
-	fi; unset runtime_dependency_checking_result
-	return 0
-}
-
-process_commandline_parameters() {
-	if [ "${#RUNTIME_COMMANDLINE_PARAMETERS[@]}" -eq 0 ]; then
-		return 0
-	fi
-
-	# modifyable parameters for parsing by consuming
-	local -a parameters=("${RUNTIME_COMMANDLINE_PARAMETERS[@]}")
-
-	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
-	local enable_debug=N
-
-	while true; do
-		if [ "${#parameters[@]}" -eq 0 ]; then
-			break
-		else
-			case "${parameters[0]}" in
-				"--help"\
-				|"-h")
-					print_help;
-					exit 0
-					;;
-				"--debug"\
-				|"-d")
-					enable_debug="Y"
-					;;
-				*)
-					printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
-					return 1
-					;;
-			esac
-			# shift array by 1 = unset 1st then repack
-			unset "parameters[0]"
-			if [ "${#parameters[@]}" -ne 0 ]; then
-				parameters=("${parameters[@]}")
-			fi
-		fi
-	done
-
-	if [ "${enable_debug}" = "Y" ]; then
-		trap 'trap_return "${FUNCNAME[0]}"' RETURN
-		set -o xtrace
-	fi
-	return 0
-}; declare -fr process_commandline_parameters;
 
 init "${@}"
 
 ## This script is based on the GNU Bash Shell Script Template project
 ## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
 ## and is based on the following version:
-## META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v2.0.1-2-g877ada8-dirty"
+## GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v3.0.16-1-g9d1ae36"
 ## You may rebase your script to incorporate new features and fixes from the template
